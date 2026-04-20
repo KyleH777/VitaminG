5402df0ebeec202efa63eb550550dd231cfebc52
Kyle <KileHarrington@gmail.com>
2026-04-20 12:11:55 -0400

    test(10-01): add PublicProfileViewModelTests stub for Plan 02 ViewModel tests
    - Create PublicProfileViewModelTests.swift with XCTest + CloudKit imports
    - All PublicProfileViewModel references commented out (type not yet created)
    - Documents 4 test cases to be implemented in Plan 02
    - File does not break the build

---
+++ VitaminG/VitaminG/VitaminGTests/PublicProfileViewModelTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+import CloudKit
+@testable import VitaminG
+
+@MainActor
+final class PublicProfileViewModelTests: XCTestCase {
+
+    // var sut: PublicProfileViewModel!  // PublicProfileViewModel will be created in Plan 02
+
+    override func setUp() async throws {
+        // PublicProfileViewModel will be created in Plan 02
+        // sut = PublicProfileViewModel()
+    }
+
+    override func tearDown() async throws {
+        // sut = nil
+    }
+
+    // MARK: - Tests to be implemented in Plan 02
+
+    // test_fetchProfile_success_transitionsToLoaded
+    // test_fetchProfile_unknownItem_transitionsToError
+    // test_fetchProfile_networkFailure_transitionsToError
+    // test_initialState_isLoading
+}
